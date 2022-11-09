87969cfca38487377d92cb5582e054dcdccdbcbe
Sandeepa <sandeepaathauda2002@gmail.com>
2022-11-09 23:36:34 +0530

    Added test endpoint

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/",function(request, response){
     response.end("Hello Sani!!");
 });
 
+app.get("/test",function(request, response){
+    response.end("This is the test endpoint");
+});
+
 app.listen(port, function(){
     console.log("Server listening on port "+port);
 });
